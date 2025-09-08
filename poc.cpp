fafb21ec1e77f68509c97a0f22771fa11deab12d
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-08 23:19:04 +0100

    removing sub because

--- poc.cpp
+++ poc.cpp
@@ -12,31 +12,13 @@ using namespace lispy;
 
 static constexpr jute::view src = R"(
   (add 3 5)
-  (add (add 1 1) (sub 2 1))
+  (add (add 1 1) (add 2 1))
 )";
 
 struct custom_node : node {
   int val;
   bool is_val;
 };
-
-struct ctx : context {
-  custom_node * sub(auto n, auto a, auto b) {
-    auto * nn = clone<custom_node>(this, n);
-    nn->val = a->val + b->val;
-    nn->is_val = true;
-    return nn;
-  }
-};
-
-const node * fn(context & ctx, const node * n, const node * const * aa, unsigned as) {
-  if (as != 2) lispy::err(n, "sub expects two coordinates");
-
-  return static_cast<struct ctx &>(ctx).sub(static_cast<const custom_node *>(n),
-    eval<custom_node>(ctx, aa[0]),
-    eval<custom_node>(ctx, aa[1])
-  );
-} 
 
 void run() {
   ctx_w_mem<custom_node> cm {};
@@ -54,7 +36,6 @@ void run() {
     nn->is_val = true;
     return nn;
   };
-  cm.ctx.fns["sub"] = fn;
 
   run(src, cm.ctx, [&](auto * node) {
     auto nn = static_cast<const custom_node *>(node);
